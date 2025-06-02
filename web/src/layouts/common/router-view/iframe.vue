acd2b0b1e7bfba89003026f309b2cdec452d8f73
妙码生花 <18523774412@qq.com>
2025-06-02 17:14:27 +0800

    refactor:优化替换已废弃的 frameborder 属性 !185

--- web/src/layouts/common/router-view/iframe.vue
+++ web/src/layouts/common/router-view/iframe.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="iframe-main" v-loading="state.loading">
-        <iframe :src="state.iframeSrc" :style="iframeStyle(35)" style="border: none;"  height="100%" width="100%" id="iframe" @load="hideLoading"></iframe>
+        <iframe :src="state.iframeSrc" :style="iframeStyle(35)" height="100%" width="100%" id="iframe" @load="hideLoading"></iframe>
     </div>
 </template>
 
 <script setup lang="ts">
 import { reactive } from 'vue'
-import { mainHeight as iframeStyle } from '/@/utils/layout'
 import { useRouter } from 'vue-router'
+import { mainHeight as iframeStyle } from '/@/utils/layout'
 
 const router = useRouter()
 
@@ -24,5 +24,8 @@ const hideLoading = () => {
 <style scoped lang="scss">
 .iframe-main {
     margin: var(--ba-main-space);
+    iframe {
+        border: none;
+    }
 }
 </style>
